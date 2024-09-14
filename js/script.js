3f9d331b98af8bd391ff87a2419ada58cb5bf190
liaakter736 <liaakter736@gmail.com>
2024-09-14 14:45:14 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+// 33-39  = D
+// 40-49  = C
+// 50-59  = B
+// 60-69  = A-
+// 70-79  = A
+// 80-100 = A+
+
+// Input
+// let marks =50
+// Input End
+// logic or software
+// if(mark =<0 || marks >100){
+// console.log("");
+
+// }
+// else if(marks >=33 && marks < 40){
+//   console.log("You Got D");
+  
+// }
+//  else if(marks >=40 && marks < 50){
+//   console.log("You Got C");
+  
+// }
+// else if(marks >=50 && marks < 60){
+//   console.log("YOu Got B");
+  
+// }
+// else if(marks >=60 && marks < 70){
+//   console.log("You Got A-");
+  
+// }
+// else if(marks >=70 && marks < 80){
+//   console.log("You Got A");
+  
+// }
+// else if(marks >=80 && marks <= 100){
+//   console.log("You Got A+");
+  
+// }
+// else{
+//   console.log("You Are Failed");
+  
+// }
+// logic or software
+
+let emailAddress = "liaakter@gmail.com"
+let cell = 12345678912
+let password = "abc123"
+
+
+if(emailAddress =="liaakter@gmail.com"){
+if( cell == 12345678912){
+  if(password == "abc123"){
+  console.log("Welcome To Ameen Book");
+  
+  }
+  else{
+    console.log("password Invalid");
+    
+  }
+}
+else{
+  console.log("Number Invalid");
+  
+}
+}
+else{
+  console.log("Invalid Email Address");
+  
+}
+// namota Star
+// let namotaNumber = 15
+// for(let i=1; i<=10; i++){
+//   let result = namotaNumber + " X " + i + " = " + namotaNumber*i
+// console.log( result);
+
+// }
+// namota Start t 
